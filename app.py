9d71a947820ce47b825f7c7f4676e9493ce79c17
Riddhi Battu <riddhibattu@gmail.com>
2025-02-03 17:20:19 +0530

    hosting

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import sys
 from pathlib import Path
 from shiny import ui, App
 import os
+
+from starlette.applications import Starlette
+from starlette.routing import Mount, Route
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
+sys.dont_write_bytecode = True
 
 from custom_ui.details_tabs import details
 from custom_ui.homepage_tabs import homepage
@@ -28,4 +35,22 @@ app_ui=ui.page_fluid(
 
 
 static_dir = Path(__file__).parent / "data"
-app = App(app_ui, server_app, static_assets=static_dir)
+shiny_app = App(app_ui, server_app, static_assets=static_dir)
+
+# Redirect root to your shiny app
+async def redirect_handler(request):
+    return RedirectResponse(url="/shiny")
+
+# Set up routes
+routes = [
+    Route("/", endpoint=redirect_handler),
+    Mount("/shiny", app=shiny_app)
+]
+
+# Create the Starlette app
+app = Starlette(routes=routes)
+app.add_middleware(SessionMiddleware, secret_key="feur")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app:app", host='0.0.0.0', port=8000)
